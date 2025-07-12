fe10d4ef461e0d72425b3f71d8cea09b1568a489
Solnyshco-gif <shtyrbus@yandex.ru>
2025-07-12 14:44:49 +0300

    # This is a combination of 2 commits. # This is the 1st commit message:
    Практическая работа 3. Знакомство с БД и ORM Hibernate.
    
    # This is the commit message #2:
    
    Практическое задание 2. Реализовал страницу Hello, world.
    
    Initial clean commit
    
    Практическое задание 3. Реализовал БД и ORM Hibernate
    
    Практическое задание 3. Реализовал БД и ORM Hibernate
    
    Практическое задание 3. Реализовал БД и ORM Hibernate
    
    Удалены файлы из ветки practical_task_3
    
    Перенесены все файлы из practical_task_2
    
    Initial clean commit
    
    Практическое задание 2. Реализовал страницу Hello, world.
    
    Практическое задание 2. Реализовал страницу Hello, world.
    
    Практическое задание 2. Реализовал страницу Hello, world.
    
    Очистка main от изменений, связанных с practical_task_3
    
    Очистка ветки main: оставлены только README.md и .gitignore
    
    Практическое задание 3. Работа с Hibernate
    
    Практическое задание 3: реализация логики через ORM Hibernate
    
    Практическое задание 3: добавлены финальные файлы и логика проекта
    
    Практическая работа 4. Знакомство с БД и ORM Hibernate.

---
+++ src/main/java/ru/Sergey/tgBot/TgBotApplication.java
@@ -0,0 +1,15 @@
+package ru.Sergey.tgBot;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class TgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/Sergey/tgBot/entity/Product.java
@@ -0,0 +1,73 @@
+package ru.Sergey.tgBot.entity;
+import jakarta.persistence.*;
+
+@Table(name = "products")
+public class Product {
+    private Long id;
+
+    private Category category;
+
+    private String name;
+
+    private String description;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Category category;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String name;
+
+    @Column(nullable = false, length = 400)
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
